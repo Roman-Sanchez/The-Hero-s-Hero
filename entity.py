a678f3126ffca3545625a577470ed52e84981527
dallinbeutler <beu12006@byui.edu>
2015-11-18 15:29:21 -0700

    Got base movement working

--- entity.py
+++ entity.py
@@ -3,10 +3,8 @@ import math
 from pygame import *
 
 
-class Entity():
+class Entity(Rect):
     def __init__(self):
-        self.locX = 0
-        self.locY = 0
         self.velX = 0
         self.velY = 0
         self.hasPhysics = True
@@ -14,14 +12,13 @@ class Entity():
         self.image = Surface((32, 32))
         self.image.convert()
         self.image.fill(Color("#FF0000"))
-        self.rect = Rect(self.locX, self.locY, 32, 32)
-
+        self = Rect(0, 0, 32, 32)
+    '''
     def __init__(self, x,y ,velx, vely, isStatic, image):
-        self.locX = x
-        self.locY = y
         self.velX = velx
         self.velY = vely
         self.hasPhysics = isStatic
         self.onGround = False
         self.image = image
-        self.rect = Rect(self.locX, self.locY, 32, 32)
+        self.rect = Rect(x, y, 32, 32)
+    '''
